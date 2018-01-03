aadbe3d0d0cfe0f577134ecf7b900e10f4d1ea29
Chris <christiangallegosd@gmail.com>
2018-01-02 19:33:21 -0800

    changes to source folder

--- src/app.jsx
+++ src/app.jsx
@@ -40,10 +40,10 @@ class App extends Component {
   calculate() {
     console.log("test");
 
-    let moneyDue = parseFloat(
-      this.state.amountReceived - this.state.amountDue).toFixed(2);
-      console.log(moneyDue);
-      //console.log(moneyDue /.01);
+  let moneyDue = parseFloat(
+    this.state.amountReceived - this.state.amountDue).toFixed(2);
+    console.log(moneyDue);
+    //console.log(moneyDue /.01);
 
     let twent;
     let ten;
@@ -53,185 +53,163 @@ class App extends Component {
     let dim;
     let nick;
     let pen = 0;
-    
 
-    // Twenties
-    if (moneyDue >= 20) {
-      twent = Math.floor(moneyDue / 20);
-      moneyDue = moneyDue % 20;
-    } else if (moneyDue < 20) {
-      twent = Math.floor(moneyDue / 20);
-      moneyDue = moneyDue % 20;
+      // Twenties
+      if (moneyDue >= 20) {
+        twent = Math.floor(moneyDue / 20);
+        moneyDue %= 20;
+      }
+
+      // Tens
+      if (moneyDue >= 10 && moneyDue < 20) {
+        ten = Math.floor(moneyDue / 10);
+        moneyDue %= 10;
+      }
+
+      // Fives
+      if (moneyDue >= 5 && moneyDue < 10) {
+        five = Math.floor(moneyDue / 5);
+        moneyDue %= 5;
+      }
+
+      // Ones
+      if (moneyDue >= 1 && moneyDue < 5) {
+        one = Math.floor(moneyDue / 1);
+        moneyDue %= 1;
+      }
+
+      // Quarters
+      if (moneyDue >= 0.25 && moneyDue < 1) {
+        quart = Math.floor(moneyDue / 0.25);
+        moneyDue %= 0.25;
+      }
+
+      // Dimes
+      if (moneyDue >= 0.1 && moneyDue < 0.25) {
+        dim = Math.floor(moneyDue / 0.1);
+        moneyDue %= 0.1;
+      }
+
+      // Nickels
+      if (moneyDue >= 0.05 && moneyDue < 0.1) {
+        nick = Math.floor(moneyDue / 0.05);
+        moneyDue %= 0.05;
+      }
+
+      // Pennies
+      if (moneyDue >= 0.01 && moneyDue < 0.05) {
+        pen = Math.round(moneyDue / 0.01);
+      }
+
+      this.setState({
+        twenties: twent,
+        tens: ten,
+        fives: five,
+        ones: one,
+        quarters: quart,
+        dimes: dim,
+        nickels: nick,
+        pennies: pen,
+        changeToCustomer: moneyDue
+      });
     }
 
-    // Tens
-    if (moneyDue >= 10) {
-      ten = Math.floor(moneyDue / 10);
-      moneyDue = moneyDue % 10;
-    } else if (moneyDue < 10) {
-      ten = Math.floor(moneyDue / 10);
-      moneyDue = moneyDue % 10;
-    }
-
-    // Fives
-    if (moneyDue >= 5) {
-      five = Math.floor(moneyDue / 5);
-      moneyDue = moneyDue % 5;
-    } else if (moneyDue < 5) {
-      five = Math.floor(moneyDue / 5);
-      moneyDue = moneyDue % 5;
-    }
-
-    // Ones
-    if (moneyDue >= 1) {
-      one = Math.floor(moneyDue / 1);
-      moneyDue = moneyDue % 1;
-    } else if (moneyDue < 1) {
-      one = Math.floor(moneyDue / 1);
-      moneyDue = moneyDue % 1;
-    }
-
-    // Quarters
-    if (moneyDue >= 0.25) {
-      quart = Math.floor(moneyDue / 0.25);
-      moneyDue = moneyDue % 0.25;
-    } else if (moneyDue < 0.25) {
-      quart = Math.floor(moneyDue / 0.25);
-      moneyDue = moneyDue % 0.25;
-    }
-
-    // Dimes
-    if (moneyDue >= 0.1) {
-      dim = Math.floor(moneyDue / 0.1);
-      moneyDue = moneyDue % 0.1;
-    } else if (moneyDue < 0.1) {
-      dim = Math.floor(moneyDue / 0.1);
-      moneyDue = moneyDue % 0.1;
-    }
-
-    // Nickels
-    if (moneyDue >= 0.05) {
-      nick = Math.floor(moneyDue / 0.05);
-      moneyDue = moneyDue % 0.05;
-    } else if (moneyDue < 0.05) {
-      nick = Math.floor(moneyDue / 0.05);
-      moneyDue = moneyDue % 0.05;
-    }
-
-   // Pennies
-    if (moneyDue <= 0.05) {
-      pen = Math.round(moneyDue / 0.01);
-    }
-
-    this.setState({
-      twenties: twent,
-      tens: ten,
-      fives: five,
-      ones: one,
-      quarters: quart,
-      dimes: dim,
-      nickels: nick,
-      pennies: pen,
-      changeToCustomer: moneyDue
-    });
-  }
-
-  render() {
-    return (
-      <div className="container">
-        <h2 className="text-white">Change Calculator</h2>
-        <hr />
-        <div className="row">
-          <div className="col-4">
-            <div className="card" className="card text-left">
-              <h6 className="card-header">Enter Information</h6>
-              <div className="card-block">
-                <h6 className="card-title">How much is due?</h6>
-                <input
-                  onChange={this.getAmountDue}
-                  type="text"
-                  id="amount-due"
-                  name="amountDue"
-                  placeholder="0"
-                />
-                <h6 className="card-title-1"> How much was received?</h6>
-                <input
-                  onChange={this.getAmountReceived}
-                  type="text"
-                  id="amount-received"
-                  name="amountReceived"
-                  placeholder="0"
-                />
-                <div className="card-footer">
-                  <button
-                    onClick={this.calculate}
-                    className="btn btn-primary btn-block"
-                  >
-                    Calculate
+    render() {
+      return (
+        <div className="container">
+          <h2 className="text-white">Change Calculator</h2>
+          <hr />
+          <div className="row">
+            <div className="col-4">
+              <div className="card" className="card text-left">
+                <h6 className="card-header">Enter Information</h6>
+                <div className="card-block">
+                  <h6 className="card-title">How much is due?</h6>
+                  <input
+                    onChange={this.getAmountDue}
+                    type="text"
+                    id="amount-due"
+                    name="amountDue"
+                    placeholder="0"
+                  />
+                  <h6 className="card-title-1"> How much was received?</h6>
+                  <input
+                    onChange={this.getAmountReceived}
+                    type="text"
+                    id="amount-received"
+                    name="amountReceived"
+                    placeholder="0"
+                  />
+                  <div className="card-footer">
+                    <button
+                      onClick={this.calculate}
+                      className="btn btn-primary btn-block"
+                    >
+                      Calculate
                   </button>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
 
-          <div className="col-8">
-            <div className="card">
-              <div className="card-block">
-                <div className="alert alert-success text-center" name = "status">
-                  The total change due is ${(this.state.amountReceived - this.state.amountDue).toFixed(2)}
-                </div>
-                {/* <div className="alert alert-danger" name = "status">
+            <div className="col-8">
+              <div className="card">
+                <div className="card-block">
+                  <div className="alert alert-success text-center" name="status">
+                    The total change due is ${(this.state.amountReceived - this.state.amountDue).toFixed(2)}
+                  </div>
+                  {/* <div className="alert alert-danger" name = "status">
                   The total money owed to customer is
                 </div> */}
 
-                <div className="row">
-                  <div className="well col-3 text-center card">
-                    <h4>Twenties</h4>
-                    <p className="lead">{this.state.twenties}</p>
-                  </div>
+                  <div className="row">
+                    <div className="well col-3 text-center card">
+                      <h4>Twenties</h4>
+                      <p className="lead">{this.state.twenties}</p>
+                    </div>
 
-                  <div className="well col-3 text-center card">
-                    <h4>Tens</h4>
-                    <p className="lead">{this.state.tens}</p>
-                  </div>
+                    <div className="well col-3 text-center card">
+                      <h4>Tens</h4>
+                      <p className="lead">{this.state.tens}</p>
+                    </div>
 
-                  <div className="well col-3 text-center card">
-                    <h4>Fives</h4>
-                    <p className="lead">{this.state.fives}</p>
-                  </div>
+                    <div className="well col-3 text-center card">
+                      <h4>Fives</h4>
+                      <p className="lead">{this.state.fives}</p>
+                    </div>
 
-                  <div className="well col-3 text-center card">
-                    <h4>Ones</h4>
-                    <p className="lead">{this.state.ones}</p>
-                  </div>
+                    <div className="well col-3 text-center card">
+                      <h4>Ones</h4>
+                      <p className="lead">{this.state.ones}</p>
+                    </div>
 
-                  <div className="well col-3 text-center card">
-                    <h4>Quarters</h4>
-                    <p className="lead">{this.state.quarters}</p>
-                  </div>
+                    <div className="well col-3 text-center card">
+                      <h4>Quarters</h4>
+                      <p className="lead">{this.state.quarters}</p>
+                    </div>
 
-                  <div className="well col-3 text-center card">
-                    <h4>Dimes</h4>
-                    <p className="lead">{this.state.dimes}</p>
-                  </div>
+                    <div className="well col-3 text-center card">
+                      <h4>Dimes</h4>
+                      <p className="lead">{this.state.dimes}</p>
+                    </div>
 
-                  <div className="well col-3 text-center card">
-                    <h4>Nickels</h4>
-                    <p className="lead">{this.state.nickels}</p>
-                  </div>
+                    <div className="well col-3 text-center card">
+                      <h4>Nickels</h4>
+                      <p className="lead">{this.state.nickels}</p>
+                    </div>
 
-                  <div className="well col-3 text-center card">
-                    <h4>Pennies</h4>
-                    <p className="lead">{this.state.pennies}</p>
+                    <div className="well col-3 text-center card">
+                      <h4>Pennies</h4>
+                      <p className="lead">{this.state.pennies}</p>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-    );
+      );
+    }
   }
-}
 
 export default App;
